2bc47a4aa0898764351768e085f52794c53efe2c
Shaalan Marwan <salsanjary@gmail.com>
2021-07-20 04:14:55 +0300

    feat: axios configuration

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,14 +1,37 @@
 import { AxiosHttpClient } from './axios-http-client'
 import axios from 'axios'
 import faker from 'faker'
+import { HttpPostClientParams } from '@/data/protocols/http'
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
-describe('AxiosHttpClient', () => {
-  test('should call axios with current Url ', async () => {
-    const url = faker.internet.url()
 
-    const sut = new AxiosHttpClient()
-    await sut.post({ url })
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+const mockedAxiosResult = {
+  data: faker.random.objectElement(),
+  status: faker.datatype.number
+}
+mockedAxios.post.mockResolvedValue(mockedAxiosResult)
+
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient()
+}
+const mockPostRequest = (): HttpPostClientParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.objectElement()
+})
+describe('AxiosHttpClient', () => {
+  test('should call axios with correct values ', async () => {
+    const request = mockPostRequest()
+
+    const sut = makeSut()
+    await sut.post(request)
+    expect(mockedAxios).toHaveBeenCalledWith(request.url, request.body)
+  })
+  test('should call axios with correct statusCode and body ', async () => {
+    const sut = makeSut()
+    const httpResponse = await sut.post(mockPostRequest())
+    expect(httpResponse).toEqual({
+      statusCode: mockedAxiosResult.status,
+      body: mockedAxiosResult.data
+    })
   })
 })
--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,7 +1,11 @@
-import { HttpPostClientParams } from '@/data/protocols/http'
+import { HttpPostClient, HttpPostClientParams, HttpResponse } from '@/data/protocols/http'
 import axios from 'axios'
-export class AxiosHttpClient {
-  async post (params: HttpPostClientParams<any>): Promise<any> {
-    await axios(params.url)
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post (params: HttpPostClientParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios(params.url, params.body)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
