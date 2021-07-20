910b8e2798cc15df6639976962356dac57d8165c
Shaalan Marwan <salsanjary@gmail.com>
2021-07-21 01:44:31 +0300

    chore: config scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,51 @@
-const path =require('path')
-const {CleanWebpackPlugin} =require('clean-webpack-plugin')
+const path = require('path')
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 module.exports = {
-    mode:'development',
-    entry:'./src/main/index.tsx',
-    output:{
-        path:path.join(__dirname,'public/js'),
-        publicPath:'/public/js',
-        filename:'bundle.js'
+    mode: 'development',
+    entry: './src/main/index.tsx',
+    output: {
+        path: path.join(__dirname, 'public/js'),
+        publicPath: '/public/js',
+        filename: 'bundle.js'
     },
-    resolve:{
-        extensions:['.ts','.tsx','js'],
-        alias:{
-            '@':path.join(__dirname,'src'),
+    resolve: {
+        extensions: ['.ts', '.tsx', 'js', 'scss'],
+        alias: {
+            '@': path.join(__dirname, 'src'),
         },
     },
-    devServer:{
-        contentBase:'/public',
-        writeToDesk:true,
-        historyApiFallback:true,
+    module: {
+        rules: [{
+            test: /\.ts(x?)$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.scss$/,
+            use: [{
+                loader: 'style-loader',
+            }, {
+                loader: 'css-loader',
+                options: {
+                    modules: true,
+                }
+            }, {
+                loader: 'sass-loader'
+            }],
+        }, {}]
+    },
+    devServer: {
+        contentBase: '/public',
+        writeToDesk: true,
+        historyApiFallback: true,
 
     },
-    externals:{
-        react:"React",
-        'react-dom':'ReactDom'
+    externals: {
+        react: "React",
+        'react-dom': 'ReactDom'
     },
-    plugins:[
+    plugins: [
         new CleanWebpackPlugin()
     ]
 }
