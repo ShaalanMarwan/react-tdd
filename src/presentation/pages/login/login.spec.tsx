ebbaf1abda7ca1fba5f25861817cda22d4237ea8
Shaalan Marwan <salsanjary@gmail.com>
2021-07-25 01:19:24 +0300

    feat: ensure login navigate to main page on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -23,7 +23,7 @@ type SutParams = {
   validationError: string;
 };
 
-const history = createMemoryHistory();
+const history = createMemoryHistory({initialEntries:['/login']});
 
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub();
@@ -180,6 +180,8 @@ describe("Login Component", () => {
       "accessToken",
       authenticationSpy.account.accessToken
     );
+    expect(history.length).toBe(1);
+    expect(history.location.pathname).toBe("/");
   });
   test("should go to sign up page", () => {
     const { sut } = makeSut();
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -9,7 +9,7 @@ import {
 import Context from "@/presentation/contexts/form/form-context";
 import { Validation } from "@/presentation/protocols/validation";
 import { Authentication } from "@/domain/usecases";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 type Props = {
   validation: Validation;
@@ -17,6 +17,8 @@ type Props = {
 };
 
 const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
+  const history=useHistory();
+
   const [state, setState] = useState({
     isLoading: false,
     email: "",
@@ -48,6 +50,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
         password: state.password,
       });
       localStorage.setItem("accessToken", account.accessToken);
+      history.push("/");
     } catch (error) {
       setState({
         ...state,
